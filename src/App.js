f16eafb962d0cefebd45787b34da319f58b1803b
Robin Stewart <>
2022-08-14 20:09:53 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [displayValue, setDisplayValue] = useState('');
+  const [activeFunction, setActiveFunctionVariable] = useState({
+    func: '',
+    firstValue: undefined
+  });
+  const buttonsRow1 = ['7', '8', '9'];
+  const buttonsRow2 = ['4', '5', '6'];
+  const buttonsRow3 = ['1', '2', '3'];
+  const buttonsRow4 = ['0', '.'];
+  const functions = {
+    Addition: 'addition',
+    Subtraction: 'subtraction',
+    Multiplication: 'multiplication',
+    Division: 'division'
+  }
+
+  function addNumber(num) {
+    setDisplayValue(displayValue + num);
+  }
+
+  function clearDisplayValue() {
+    setDisplayValue('');
+  }
+
+  function convertPositiveToNegative() {
+    setDisplayValue(displayValue * -1);
+  }
+
+  function setActiveFunction(func) {
+    setActiveFunctionVariable({
+      func: func,
+      firstValue: displayValue
+    });
+    clearDisplayValue();
+  }
+
+  function calculateResult() {
+    switch (activeFunction.func) {
+      case functions.Addition:
+        setDisplayValue(parseInt(activeFunction.firstValue) + parseInt(displayValue))
+        break;
+      case functions.Subtraction:
+        setDisplayValue(parseInt(activeFunction.firstValue) - parseInt(displayValue))
+        break;
+      case functions.Multiplication:
+        setDisplayValue(parseInt(activeFunction.firstValue) * parseInt(displayValue))
+        break;
+      case functions.Division:
+        setDisplayValue(parseInt(activeFunction.firstValue) / parseInt(displayValue))
+        break;
+      default:
+        console.log('Error calculating the calculator result.')
+        break;
+    }
+    setActiveFunctionVariable({
+      func: '',
+      firstValue: undefined
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Calculator
+        </h1>
       </header>
+      <div className='body'>
+        <div className='display-value'>
+          {displayValue}
+        </div>
+        <br></br>
+        <button className='large-button' type="button" onClick={clearDisplayValue}>Clear</button>
+        <button type="button" onClick={convertPositiveToNegative}>+/-</button>
+        <button type="button" onClick={() => setActiveFunction(functions.Addition)}
+          className={activeFunction.func === functions.Addition ? 'selected' : null}>+</button>
+        <br></br>
+        {buttonsRow1.map((number, index) => (
+          <button type="button" key={index} onClick={() => addNumber(number)}>{number}</button>
+        ))}
+        <button type="button" onClick={() => setActiveFunction(functions.Subtraction)}
+          className={activeFunction.func === functions.Subtraction ? 'selected' : null}>-</button>
+        <br></br>
+        {buttonsRow2.map((number, index) => (
+          <button type="button" key={index} onClick={() => addNumber(number)}>{number}</button>
+        ))}
+        <button type="button" onClick={() => setActiveFunction(functions.Multiplication)}
+          className={activeFunction.func === functions.Multiplication ? 'selected' : null}>x</button>
+        <br></br>
+        {buttonsRow3.map((number, index) => (
+          <button type="button" key={index} onClick={() => addNumber(number)}>{number}</button>
+        ))}
+        <button type="button" onClick={() => setActiveFunction(functions.Division)}
+          className={activeFunction.func === functions.Division ? 'selected' : null}>÷</button>
+        <br></br>
+        {buttonsRow4.map((number, index) => (
+          <button type="button" key={index} onClick={() => addNumber(number)}>{number}</button>
+        ))}
+        <button className='large-button' type="button" onClick={calculateResult}>=</button>
+      </div>
     </div>
   );
 }
